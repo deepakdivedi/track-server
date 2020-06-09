518d6c7100367e6370ba8d0364a3d83ae663d547
Deepak Divedi <deepakdivedi@gmail.com>
2020-06-09 14:28:46 +0530

    Project complete

---
+++ src/Index.js
@@ -0,0 +1,39 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+  "mongodb+srv://admin:dbPassword@cluster0-zcjiq.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo Instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to Mongo ", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email : ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on Port 3000");
+});
